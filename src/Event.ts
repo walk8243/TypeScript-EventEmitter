c3d9fe7b031de68dea2ca8fd189e2b88060366f4
walk8243 <walk8243@gmail.com>
2019-08-04 19:07:40 +0900

    emitやonにVSCodeの入力補完が効くように構築

--- src/Event.ts
+++ src/Event.ts
@@ -3,8 +3,28 @@ import { EventEmitter } from "events";
 export default class Event extends EventEmitter {
   constructor() {
     super();
-    this.on('event', () => {
-      console.log('event');
-    });
+    this.init();
+  }
+
+  init() {
+    this.setEventListener();
+  }
+
+  setEventListener() {
+    this.on('event', this.onEvent);
+  }
+
+  onEvent() {
+    console.log('event');
+  }
+
+  emit(event: 'event'): boolean;
+  emit(event: string | symbol, ...args: any[]) {
+    return super.emit(event, ...args);
+  }
+
+  on(event: 'event', listener: () => void): this;
+  on(event: string | symbol, listener: (...args: any[]) => void) {
+    return super.on(event, listener);
   }
 }
