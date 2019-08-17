f4ce356f2d5a18b496c7a996ca2a220331026eaa
walk8243 <walk8243@gmail.com>
2019-08-17 23:51:06 +0900

    event部分で `|` を使用

--- src/Event.ts
+++ src/Event.ts
@@ -6,6 +6,7 @@ export default class Event extends EventEmitter {
 
     this.on('keyboard', this.eventListenerKeyboard);
     this.on('enter', this.eventListenerEnter);
+    this.on('escape', () => {});
     this.on('mouse', this.eventListenerMouse);
   }
 
@@ -20,18 +21,18 @@ export default class Event extends EventEmitter {
   }  
 
   on(event: 'keyboard', listener: Event['eventListenerKeyboard']): this;
-  on(event: 'enter', listener: Event['eventListenerEnter']): this;
+  on(event: 'enter' | 'escape', listener: Event['eventListenerEnter']): this;
   on(event: 'mouse', listener: Event['eventListenerMouse']): this;
   on(event: EventType, listener: (...args: any[]) => void) {
     return super.on(event, listener);
   }
 
   emit(event: 'keyboard'): boolean;
-  emit(event: 'enter'): boolean;
+  emit(event: 'enter' | 'escape'): boolean;
   emit(event: 'mouse', x: number, y: number): boolean;
   emit(event: EventType, ...args: any[]) {
     return super.emit(event, ...args);
   }
 }
 
-type EventType = 'keyboard' | 'enter' | 'mouse';
+type EventType = 'keyboard' | 'enter' | 'escape' | 'mouse';
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ const event = new Event();
 event.emit('keyboard');
 event.emit('mouse', 10, 100);
 event.emit('enter');
+event.emit('escape');
