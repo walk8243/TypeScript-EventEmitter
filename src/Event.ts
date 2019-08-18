e2037f41aa272cfba019eee333a051c50c649904
walk8243 <walk8243@gmail.com>
2019-08-18 21:48:42 +0900

    型定義でまとめられるところをまとめる

--- src/Event.ts
+++ src/Event.ts
@@ -20,15 +20,13 @@ export default class Event extends EventEmitter {
     console.log(x, y);
   }  
 
-  on(event: 'keyboard', listener: () => void): this;
-  on(event: 'enter' | 'escape', listener: () => void): this;
+  on(event: 'keyboard' | 'enter' | 'escape', listener: () => void): this;
   on(event: 'mouse', listener: (mouseX: number, mouseY: number) => void): this;
   on(event: EventType, listener: (...args: any[]) => void) {
     return super.on(event, listener);
   }
 
-  emit(event: 'keyboard'): boolean;
-  emit(event: 'enter' | 'escape'): boolean;
+  emit(event: 'keyboard' | 'enter' | 'escape'): boolean;
   emit(event: 'mouse', x: number, y: number): boolean;
   emit(event: EventType, ...args: any[]) {
     return super.emit(event, ...args);
